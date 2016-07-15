3203119995c8f81557df80ae40ab2b4397550094
dragonfly95 <nosignal95@gmail.com>
2016-07-15 13:56:45 +0900

    Update PersonServiceImpl.java

--- src/main/java/com/dom925/demo/spring/hidernate/service/PersonServiceImpl.java
+++ src/main/java/com/dom925/demo/spring/hidernate/service/PersonServiceImpl.java
@@ -1,1 +1,28 @@
+package com.dom925.demo.spring.hibernate.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import com.dom925.demo.spring.hibernate.dao.PersonDAO;
+import com.dom925.demo.spring.hibernate.model.Person;
+
+
+@Service
+public class PersonServiceImpl implements PersonService {
+
+ @Autowired
+ private PersonDAO personDao;
+ 
+ public List<Person> getAllPersons() {
+  List<Person> persons = personDao.getAllPersons();
+  return persons;
+ }
+
+ public void insertPerson(Person person) {
+  personDao.insertPerson(person);
+ }
+
+}
