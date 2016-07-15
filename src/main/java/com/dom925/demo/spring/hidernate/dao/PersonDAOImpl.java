57d9813b05b1434c63a599c2c1caf8940f13bc5f
dragonfly95 <nosignal95@gmail.com>
2016-07-15 13:56:05 +0900

    Update PersonDAOImpl.java

--- src/main/java/com/dom925/demo/spring/hidernate/dao/PersonDAOImpl.java
+++ src/main/java/com/dom925/demo/spring/hidernate/dao/PersonDAOImpl.java
@@ -1,1 +1,36 @@
+package com.dom925.demo.spring.hibernate.dao;
 
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dom925.demo.spring.hibernate.model.Person;
+
+@Repository
+@Transactional(readOnly = true)
+public class PersonDaoImpl implements PersonDAO {
+
+ @Autowired
+ private SessionFactory sessionFactory;
+
+ 
+ public List<Person> getAllPersons() {
+  Session session = sessionFactory.openSession();
+  String hql = "FROM Person";
+  Query query = session.createQuery(hql);
+  List<Person> persons = query.list();
+  return persons;
+ }
+
+ @Transactional(readOnly = false)
+ public void insertPerson(Person employee) {
+  Session session = sessionFactory.openSession();
+  session.save(employee);
+ }
+
+}
