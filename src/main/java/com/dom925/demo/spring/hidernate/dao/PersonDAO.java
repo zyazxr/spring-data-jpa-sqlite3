ed722887f949895b046d1cfe194afe7625c33801
dragonfly95 <nosignal95@gmail.com>
2016-07-15 13:55:43 +0900

    Update PersonDAO.java

--- src/main/java/com/dom925/demo/spring/hidernate/dao/PersonDAO.java
+++ src/main/java/com/dom925/demo/spring/hidernate/dao/PersonDAO.java
@@ -1,1 +1,12 @@
-d
+package com.dom925.demo.spring.hibernate.dao;
+
+import java.util.List;
+
+import com.dom925.demo.spring.hibernate.model.Person;
+
+public interface PersonDAO {
+
+ public List<Person> getAllPersons();
+ 
+ public void insertPerson (Person person);
+}
