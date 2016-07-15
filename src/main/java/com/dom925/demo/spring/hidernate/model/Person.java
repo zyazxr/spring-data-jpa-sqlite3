f08e7c02f4be305dfbd27baee94256d120ff4493
dragonfly95 <nosignal95@gmail.com>
2016-07-15 13:55:16 +0900

    Update Person.java

--- src/main/java/com/dom925/demo/spring/hidernate/model/Person.java
+++ src/main/java/com/dom925/demo/spring/hidernate/model/Person.java
@@ -1,1 +1,68 @@
+package com.dom925.demo.spring.hibernate.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PERSON_INFO")
+public class Person {
+
+ @Id
+ @Column(name = "ID")
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
+ private Integer id;
+ 
+ @Column(name = "FIRST_NAME")
+ private String firstName;
+ 
+ @Column(name = "LAST_NAME")
+ private String lastName;
+  
+ public Integer getId() {
+  return id;
+ }
+ public void setId(Integer id) {
+  this.id = id;
+ }
+ public String getFirstName() {
+  return firstName;
+ }
+ public void setFirstName(String firstName) {
+  this.firstName = firstName;
+ }
+ public String getLastName() {
+  return lastName;
+ }
+ public void setLastName(String lastName) {
+  this.lastName = lastName;
+ }
+ @Override
+ public boolean equals(Object obj){
+  if (this == obj){
+   return true;
+  }
+  Person person = (Person) obj;
+  if (firstName != null ?
+    !firstName.equals(person.firstName)
+    :person.firstName != null){
+   return false;
+  }
+  else {
+   return true;
+  } 
+ }
+ @Override
+ public String toString() {
+  return "Person [id=" 
+    + id 
+    + ", name=" 
+    + firstName 
+    + " " 
+    + lastName 
+    + "]";
+ }
+}
