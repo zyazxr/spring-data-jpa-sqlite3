68631558402619c9bc2dbd0d246d733534a628c5
dragonfly95 <nosignal95@gmail.com>
2016-07-15 13:56:25 +0900

    Update PersonService.java

--- src/main/java/com/dom925/demo/spring/hidernate/service/PersonService.java
+++ src/main/java/com/dom925/demo/spring/hidernate/service/PersonService.java
@@ -1,1 +1,12 @@
+package com.dom925.demo.spring.hibernate.service;
 
+import java.util.List;
+
+import com.dom925.demo.spring.hibernate.model.Person;
+
+public interface PersonService {
+
+ public List<Person> getAllPersons();
+ 
+ public void insertPerson(Person person);
+}
